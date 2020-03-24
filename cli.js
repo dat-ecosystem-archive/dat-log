1ddb7a338a3880ec4cb8f47febb4e8abb2d0b628
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-03-24 14:50:29 -0700

    update package-lock & standard

--- cli.js
+++ cli.js
@@ -4,5 +4,5 @@ var minimist = require('minimist')
 var datLog = require('.')
 
 datLog(minimist(process.argv.slice(2), {
-  alias: { live: 'l', help: 'h' }, boolean: [ 'live', 'help' ]
+  alias: { live: 'l', help: 'h' }, boolean: ['live', 'help']
 }))
